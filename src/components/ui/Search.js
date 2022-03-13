b97350ba7a77fe140c0883e38c035f5de4e3a2dd
Adit0507 <adi05072002@gmail.com>
2022-03-13 10:31:27 +0530

    small fix

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -14,7 +14,7 @@ const Search = ({ getQuery }) => {
         <input
           type='text'
           className='form-control'
-          placeholder="Say My Name"
+          placeholder="Say My Name 😎"
           value={text}
           onChange={(e) => onChange(e.target.value)}
           autoFocus
